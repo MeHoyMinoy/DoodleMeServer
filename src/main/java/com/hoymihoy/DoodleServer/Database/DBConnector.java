7d435aee4434ef680528e33d89b592e0501d934d
Bearded-Baguette <dhg5054@gmail.com>
2018-11-20 15:16:17 -0500

    Separated User table functions. Begin adding painting controllers to database

--- src/main/java/com/hoymihoy/DoodleServer/Database/DBConnector.java
+++ src/main/java/com/hoymihoy/DoodleServer/Database/DBConnector.java
@@ -6,9 +6,9 @@ import com.hoymihoy.DoodleServer.DTOS.User;
 import java.sql.*;
 
 public class DBConnector {
-    private Connection con;
-    private Statement stmt;
-    private ResultSet rs;
+    public Connection con;
+    public Statement stmt;
+    public ResultSet rs;
     private String DBUsername = "admin";
     private String DBPassword = "DoodleMe";
     private String jdbcUrl = String.format(
@@ -17,7 +17,7 @@ public class DBConnector {
                     "direct-return-186020:us-east4:doodle-database"
     );
     
-    private Connection initializeConnection() throws SQLException {
+    protected Connection initializeConnection() throws SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             con = DriverManager.getConnection(jdbcUrl, DBUsername, DBPassword);
@@ -66,7 +66,8 @@ public class DBConnector {
             stmt.executeUpdate("create TABLE Users(userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, userName VARCHAR(100) NOT NULL UNIQUE, password VARCHAR(100) NOT NULL,"
                     + "first_name VARCHAR(100), last_name VARCHAR(100), email VARCHAR(100), birthDate DATE)");
 
-        } 
+            stmt.executeUpdate("create TABLE Paintings(paintingID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, creatorID)")
+        }
         catch (Exception ex) {
             System.out.println(ex);
             con.close();
@@ -77,115 +78,5 @@ public class DBConnector {
         }
         con.close();
         return 1;
-    }
-
-    public int createNewUser(User user) throws SQLException {
-        String updateString = "INSERT INTO Users(userName,password,first_name,last_name,email,birthDate) VALUES('" + user.getUsername() + "', '" + user.getPassword() + "', '" + user.getFirstname() + "', '"
-                + user.getLastname() + "', '" + user.getEmail() + "', '" + user.getBirthdate() + "');";
-
-        try {
-            con = initializeConnection();
-            stmt = con.createStatement();
-            int returnValue = stmt.executeUpdate(updateString);
-            con.close();
-            return returnValue;
-        }
-        catch (Exception e) {
-            System.out.println(e);
-            con.close();
-            return -1;
-        }
-        finally {
-            if (stmt != null)
-            {stmt.close();}
-        }
-    }
-
-    public User queryUserID(int userID) throws SQLException {
-        String queryString = "SELECT * FROM Users WHERE userID = '" + userID + "';";
-        User user = new User();
-
-        try {
-            con = initializeConnection();
-            stmt = con.createStatement();
-            rs = stmt.executeQuery(queryString);
-
-            while (rs.next())
-            {
-                user.setUserID(rs.getInt("userID"));
-                user.setUsername(rs.getString("userName"));
-                user.setPassword(rs.getString("password"));
-                user.setFirstname(rs.getString("first_name"));
-                user.setLastname(rs.getString("last_name"));
-                user.setEmail(rs.getString("email"));
-            }
-
-            con.close();
-            return user;
-
-        }
-        catch (Exception e) {
-            System.out.println(e);
-            user.setUsername("NULL");
-            con.close();
-            return user;
-        }
-        finally {
-            if (stmt != null)
-            {stmt.close();}
-        }
-    }
-
-    public int queryLoginCredentials(SecureUserLogin sul) throws SQLException {
-        String queryString = "SELECT userID FROM Users WHERE userName = '" + sul.getUserName() + "' AND password = '" + sul.getPassword() + "'";
-
-        int userID = 0;
-
-        try {
-            con = initializeConnection();
-            stmt = con.createStatement();
-            rs = stmt.executeQuery(queryString);
-            while(rs.next())
-            {
-                userID = rs.getInt("userID");
-                con.close();
-                return userID;
-            }
-        }
-        catch (Exception e) {
-            System.out.println(e);
-            con.close();
-            return -1;
-        }
-        finally {
-            if (stmt != null)
-            {stmt.close();}
-        }
-
-        return userID;
-    }
-
-    public int updateUser(int userID, User user) throws SQLException {
-        String updateString = "UPDATE Users SET userName = '" + user.getUsername() + "', first_name = '" + user.getFirstname()
-                + "', last_name = '" + user.getLastname() + "', email = '" + user.getEmail() + "', birthDate = '" + user.getBirthdate()
-                + "' WHERE userID = " + userID;
-
-        try {
-            con = initializeConnection();
-            stmt = con.createStatement();
-            int returnValue = stmt.executeUpdate(updateString);
-            con.close();
-
-            return returnValue;
-        }
-        catch (Exception e) {
-            System.out.println(e);
-            con.close();
-            return -1;
-        }
-        finally {
-            if (stmt != null)
-            {stmt.close();}
-        }
     }
 }
---
+++ src/main/java/com/hoymihoy/DoodleServer/Database/DB_User.java
@@ -0,0 +1,121 @@
+package com.hoymihoy.DoodleServer.Database;
+
+import com.hoymihoy.DoodleServer.DTOS.SecureUserLogin;
+import com.hoymihoy.DoodleServer.DTOS.User;
+
+import java.sql.SQLException;
+
+public class DB_User {
+
+    DBConnector DBC = new DBConnector();
+
+    public int createNewUser(User user) throws SQLException {
+        String updateString = "INSERT INTO Users(userName,password,first_name,last_name,email,birthDate) VALUES('" + user.getUsername() + "', '" + user.getPassword() + "', '" + user.getFirstname() + "', '"
+                + user.getLastname() + "', '" + user.getEmail() + "', '" + user.getBirthdate() + "');";
+
+        try {
+            DBC.con = DBC.initializeConnection();
+            DBC.stmt = DBC.con.createStatement();
+            int returnValue = DBC.stmt.executeUpdate(updateString);
+            DBC.con.close();
+            return returnValue;
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            DBC.con.close();
+            return -1;
+        }
+        finally {
+            if (DBC.stmt != null)
+            {DBC.stmt.close();}
+        }
+    }
+
+    public User queryUserID(int userID) throws SQLException {
+        String queryString = "SELECT * FROM Users WHERE userID = '" + userID + "';";
+        User user = new User();
+
+        try {
+            DBC.con = DBC.initializeConnection();
+            DBC.stmt = DBC.con.createStatement();
+            DBC.rs = DBC.stmt.executeQuery(queryString);
+
+            while (DBC.rs.next())
+            {
+                user.setUserID(DBC.rs.getInt("userID"));
+                user.setUsername(DBC.rs.getString("userName"));
+                user.setPassword(DBC.rs.getString("password"));
+                user.setFirstname(DBC.rs.getString("first_name"));
+                user.setLastname(DBC.rs.getString("last_name"));
+                user.setEmail(DBC.rs.getString("email"));
+            }
+
+            DBC.con.close();
+            return user;
+
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            user.setUsername("NULL");
+            DBC.con.close();
+            return user;
+        }
+        finally {
+            if (DBC.stmt != null)
+            {DBC.stmt.close();}
+        }
+    }
+
+    public int queryLoginCredentials(SecureUserLogin sul) throws SQLException {
+        String queryString = "SELECT userID FROM Users WHERE userName = '" + sul.getUserName() + "' AND password = '" + sul.getPassword() + "'";
+
+        int userID = 0;
+
+        try {
+            DBC.con = DBC.initializeConnection();
+            DBC.stmt = DBC.con.createStatement();
+            DBC.rs = DBC.stmt.executeQuery(queryString);
+            while(DBC.rs.next())
+            {
+                userID = DBC.rs.getInt("userID");
+                DBC.con.close();
+                return userID;
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            DBC.con.close();
+            return -1;
+        }
+        finally {
+            if (DBC.stmt != null)
+            {DBC.stmt.close();}
+        }
+
+        return userID;
+    }
+
+    public int updateUser(int userID, User user) throws SQLException {
+        String updateString = "UPDATE Users SET userName = '" + user.getUsername() + "', first_name = '" + user.getFirstname()
+                + "', last_name = '" + user.getLastname() + "', email = '" + user.getEmail() + "', birthDate = '" + user.getBirthdate()
+                + "' WHERE userID = " + userID;
+
+        try {
+            DBC.con = DBC.initializeConnection();
+            DBC.stmt = DBC.con.createStatement();
+            int returnValue = DBC.stmt.executeUpdate(updateString);
+            DBC.con.close();
+
+            return returnValue;
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            DBC.con.close();
+            return -1;
+        }
+        finally {
+            if (DBC.stmt != null)
+            {DBC.stmt.close();}
+        }
+    }
+}
